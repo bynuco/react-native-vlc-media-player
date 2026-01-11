57a8a94f3812f8c28c9655ca1807e6bfa13a6742
Nesimi Umutcan Öztürk <nesimiztrk@gmail.com>
2026-01-11 23:14:36 +0300

    a

--- expo/ios/withMobileVlcKit.js
+++ expo/ios/withMobileVlcKit.js
@@ -19,7 +19,7 @@ const withMobileVlcKit = (config, options) => {
             const newCode = generateCode.mergeContents({
                 tag: "withVlcMediaPlayer",
                 src: contents,
-                newSrc: "  pod 'VLCKit', '4.0.0a18'",
+                newSrc: "  pod 'VLCKit', '~> 4.0.0'",
                 anchor: /use\_expo\_modules\!/i,
                 offset: 3,
                 comment: "  #",
